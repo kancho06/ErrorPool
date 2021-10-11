911988c145edcdca0606493d617ec41f8fb389c7
kancho06 <wotjd5792@gmail.com>
2021-10-11 14:44:34 +0900

    10/11
    10/11

---
+++ ErrorPool/src/test/java/com/sparta/errorpool/ErrorPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.errorpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
