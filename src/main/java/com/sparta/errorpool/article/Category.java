c475b9fc238bb1dd73ba6ef1374d405a5b014de2
rockintuna <rockintuna@naver.com>
2021-10-11 17:50:33 +0900

    enum에 번호 추가

--- src/main/java/com/sparta/errorpool/article/Category.java
+++ src/main/java/com/sparta/errorpool/article/Category.java
@@ -1,15 +1,21 @@
 package com.sparta.errorpool.article;
 
 public enum Category {
-    TECH("tech"), FREE_BOARD("자유게시판"), ERROR("오류공유");
+    TECH(1,"tech"), FREE_BOARD(2,"자유게시판"), ERROR(3,"오류공유");
 
+    private final Integer num;
     private final String name;
 
-    Category(String name) {
+    private Category(Integer num, String name) {
+        this.num = num;
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Integer getNum() {
+        return num;
     }
 }
--- src/main/java/com/sparta/errorpool/article/Skill.java
+++ src/main/java/com/sparta/errorpool/article/Skill.java
@@ -1,15 +1,21 @@
 package com.sparta.errorpool.article;
 
 public enum Skill {
-    REACT("react"), SPRING("spring"), NODEJS("node.js");
+    REACT(1,"react"), SPRING(2,"spring"), NODEJS(3,"node.js");
 
+    private final Integer num;
     private final String name;
 
-    Skill(String name) {
+    private Skill(Integer num, String name) {
+        this.num = num;
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Integer getNum() {
+        return num;
     }
 }
