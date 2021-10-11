b4218a9bb744621ce5803eb2fe5227ce3a859f93
rockintuna <rockintuna@naver.com>
2021-10-11 22:56:46 +0900

    ArticleResponseDto 생성

--- src/main/java/com/sparta/errorpool/article/ArticleResponseDto.java
+++ src/main/java/com/sparta/errorpool/article/ArticleResponseDto.java
@@ -1,8 +1,21 @@
 package com.sparta.errorpool.article;
 
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Builder
 public class ArticleResponseDto {
-    //todo
+    private Long articleId;
+    private String title;
+    private String content;
+    private Integer viewCount;
+    private Integer commentCount;
+    private Integer skillId;
+    private Integer categoryId;
+    private String username;
+    private String email;
+
+    @Setter
+    private Integer like;
+
 }
