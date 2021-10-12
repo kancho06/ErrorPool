bb4a6fac580ed7df5fc5c7a9e03fb2ed04186f30
kancho06 <wotjd5792@gmail.com>
2021-10-12 15:35:53 +0900

    10/12 123
    10/12
    123

--- src/main/java/com/sparta/errorpool/globalController/GlobalController.java
+++ src/main/java/com/sparta/errorpool/globalController/GlobalController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
+
 @ControllerAdvice
 public class GlobalController {
 
