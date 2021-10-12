f3205dbd026f60435b0f7df5218a02e53166745c
kancho06 <wotjd5792@gmail.com>
2021-10-12 17:50:35 +0900

    10/12 jwt 삭제
    10/12 jwt 삭제

--- src/main/java/com/sparta/errorpool/security/UserDetailsImpl.java
+++ src/main/java/com/sparta/errorpool/security/UserDetailsImpl.java
@@ -60,8 +60,7 @@ public class UserDetailsImpl implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
 
-//
-//        return Collections.emptyList();
+
         UserRoleEnum role = user.getRole();
         String authority = role.getAuthority();
 
--- src/main/java/com/sparta/errorpool/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/errorpool/security/WebSecurityConfig.java
@@ -16,17 +16,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    private final JwtTokenProvider jwtTokenProvider;
-
 
         @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
 
 
     @Override
@@ -38,20 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        //jwt
-//        http
-//                .headers().frameOptions().sameOrigin().and()
-//                .httpBasic().disable() // REST API만을 고려, 기본 설정 해제
-//                .csrf().disable() // csrf 사용 X
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                // 토큰 기반 인증이므로 세션도 사용 X
-//                .and()
-//                .authorizeRequests() // 요청에 대한 사용권한 체크
-//                .anyRequest().permitAll() // 나머지 요청은 누구나 접근 가능
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-//                        UsernamePasswordAuthenticationFilter.class);
 
 
         http.csrf().disable();
