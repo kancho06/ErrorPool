ed5e80c189c58f5ed69c7e4d173b5789284b1e38
kancho06 <wotjd5792@gmail.com>
2021-10-14 11:45:57 +0900

    10/14 web 컨피그 위치변경
    10/14 web 컨피그 위치변경

--- src/main/java/com/sparta/errorpool/user/webConfig/WebConfig.java
+++ src/main/java/com/sparta/errorpool/user/webConfig/WebConfig.java
@@ -1,10 +1,30 @@
 package com.sparta.errorpool.user.webConfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        //프론트랑 합칠때 localhost:3000 으로 해야한대서
+                        .allowedOrigins("http://localhost:3000", "http://errorpool.s3-website.ap-northeast-2.amazonaws.com/")
+                        .maxAge(3000)
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.HEAD.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name());
+            }
+        };
+    }
 
 }
