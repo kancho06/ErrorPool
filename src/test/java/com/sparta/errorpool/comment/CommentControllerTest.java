ce1e6944413bae47b51ba3c9cdfc1686bed3aa92
rockintuna <rockintuna@naver.com>
2021-10-16 03:35:55 +0900

    Comment 테스트 정리

--- src/test/java/com/sparta/errorpool/comment/CommentControllerTest.java
+++ src/test/java/com/sparta/errorpool/comment/CommentControllerTest.java
@@ -1,7 +1,9 @@
 package com.sparta.errorpool.comment;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.errorpool.article.Article;
 import com.sparta.errorpool.article.Skill;
+import com.sparta.errorpool.article.dto.ArticleCreateRequestDto;
 import com.sparta.errorpool.security.UserDetailsImpl;
 import com.sparta.errorpool.security.WebSecurityConfig;
 import com.sparta.errorpool.user.User;
@@ -17,16 +19,19 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(
@@ -38,9 +43,6 @@ class CommentControllerTest {
     private MockMvc mvc;
 
     private Principal mockPrincipal;
-
-    @Autowired
-    private WebApplicationContext context;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -57,48 +59,50 @@ class CommentControllerTest {
 //                .apply(springSecurity(new MockSpringSecurityFilter())).build();
 //    }
 
+    User testUser;
+    Article mockArticle;
 
-        private void mockUserSetup() {
+    private void mockUserSetup() {
 // Mock 테스트 유져 생성
         String username = "namelim@gmail.com";
         String password = "1111";
         String email = "namelim@gmail.com";
         String socialId = "namelim@gmail.com";
         UserRoleEnum role = UserRoleEnum.USER;
-        User testUser = new User(email,password,username,role, Skill.SPRING);
+        testUser = new User(email,password,username,role, Skill.SPRING);
         UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
         mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
+
+        mockArticle = Article.of(
+                new ArticleCreateRequestDto
+                        ("test title 5", null, "test content 5", 2, 2), testUser);
+
     }
 
 
-    @Test
-    @WithMockUser( username="namelim@gmail.com", password = "custom_password", roles = {"USER"})
-    @DisplayName("댓글 추가-input 확인-성공")
-    void addComment_CheckInputNormal() throws Exception {
-        Map<String, Long> input = new HashMap<>();
-        input.put("articleId",21L);
-
-//        User user = this.mockUserSetup();
-        this.mockUserSetup();
-        CommentDto commentDto = CommentDto.builder()
-                .articleId(30L)
-                .commentId(1L)
-                .username("namelim@gmail.com")
-                .content("댓글1")
-                .build();
-
-        String commentInfo = objectMapper.writeValueAsString(commentDto);
-        String inputInfo = objectMapper.writeValueAsString(input);
-        mvc.perform(post("/comments")
-                        .param("articleId",String.valueOf(30L))
-                        .content(commentInfo)
-                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON)
-                        .principal(mockPrincipal)
-                )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(print());
-    }
+//    @Test
+//    @WithMockUser( username="namelim@gmail.com", password = "custom_password", roles = {"USER"})
+//    @DisplayName("댓글 추가-input 확인-성공")
+//    void addComment_CheckInputNormal() throws Exception {
+//        this.mockUserSetup();
+//        CommentDto commentDto = CommentDto.builder()
+//                .articleId(30L)
+//                .commentId(1L)
+//                .username("namelim@gmail.com")
+//                .content("댓글1")
+//                .build();
+//        String commentInfo = objectMapper.writeValueAsString(commentDto);
+//        given(commentService.addComment(eq(30L), eq(commentDto), any(User.class)))
+//                .willReturn(new Comment(testUser, mockArticle, "댓글1"));
+//
+//        mvc.perform(post("/comments")
+//                        .with(csrf())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(commentInfo))
+////                        .principal(mockPrincipal))
+//                .andDo(print())
+//                .andExpect(status().isOk());
+//    }
 
     @Test
     @DisplayName("댓글 추가-input 확인-오류발생(Request게시글 아이디가 비어있음)")
