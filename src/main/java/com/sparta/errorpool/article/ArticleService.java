850358d93be7cf810d460dbea9945cdcdc03db17
rockintuna <rockintuna@naver.com>
2021-10-15 14:29:00 +0900

    페이지 번호 수정

--- src/main/java/com/sparta/errorpool/article/ArticleService.java
+++ src/main/java/com/sparta/errorpool/article/ArticleService.java
@@ -19,11 +19,10 @@ import java.util.Optional;
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final LikeInfoRepository likeRepository;
-    private final CommentRepository commentRepository;
 
     public Page<Article> getArticlesInSkillAndCategory(Integer page, Integer skillId, Integer categoryId) {
         return articleRepository.findAllBySkillAndCategory
-                (PageRequest.of(page, 6), Skill.getSkillById(skillId), Category.getCategoryById(categoryId));
+                (PageRequest.of(page-1, 6), Skill.getSkillById(skillId), Category.getCategoryById(categoryId));
     }
 
     public Article getArticleById(Long articleId) {
